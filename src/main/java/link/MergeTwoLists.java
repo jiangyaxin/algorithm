95eb5f38e3a50ec26ca7ceb3903d54085bc72871
jiangyaxin <792124098@qq.com>
2022-04-24 22:39:25 +0800

    新增链表 142、160、141、23、876、19

--- src/main/java/link/MergeTwoLists.java
+++ src/main/java/link/MergeTwoLists.java
@@ -52,28 +52,4 @@ public class MergeTwoLists {
         }
         return head.next;
     }
-
-    public static class ListNode {
-
-        int val;
-
-        ListNode next;
-
-        public ListNode() {
-
-        }
-
-        public ListNode(int val, ListNode next) {
-            this.val = val; this.next = next;
-        }
-
-        @Override
-        public String toString(){
-            StringBuilder sb = new StringBuilder();
-            for(ListNode p = this ; p != null ; p = p.next ){
-                sb.append(p.val);
-            }
-            return sb.toString();
-        }
-   }
 }
--- src/test/java/LinkTests.java
+++ src/test/java/LinkTests.java
@@ -1,5 +1,4 @@
-import bfs.OpenLock;
-import link.MergeTwoLists;
+import link.*;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -14,16 +13,82 @@ public class LinkTests {
     @Test
     public void mergeTwoListsTest(){
         assertThat(new MergeTwoLists().mergeTwoLists(
-                new MergeTwoLists.ListNode(1,new MergeTwoLists.ListNode(2,new MergeTwoLists.ListNode(4,null))),
-                new MergeTwoLists.ListNode(1,new MergeTwoLists.ListNode(3,new MergeTwoLists.ListNode(4,null)))
+                new ListNode(1,new ListNode(2,new ListNode(4,null))),
+                new ListNode(1,new ListNode(3,new ListNode(4,null)))
         ).toString())
                 .as("mergeTwoListsTest")
                 .isEqualTo(
-                        new MergeTwoLists.ListNode(1,
-                                new MergeTwoLists.ListNode(1,
-                                        new MergeTwoLists.ListNode(2,
-                                                new MergeTwoLists.ListNode(3,
-                                                        new MergeTwoLists.ListNode(4,
-                                                                new MergeTwoLists.ListNode(4,null)))))).toString());
+                        new ListNode(1,
+                                new ListNode(1,
+                                        new ListNode(2,
+                                                new ListNode(3,
+                                                        new ListNode(4,
+                                                                new ListNode(4,null)))))).toString());
+    }
+
+    @Test
+    public void mergeKListsTest(){
+        assertThat(new MergeKLists().mergeKLists(new ListNode[]{
+                new ListNode(1, new ListNode(4, new ListNode(5, null))),
+                new ListNode(1, new ListNode(3, new ListNode(4, null))),
+                new ListNode(2, new ListNode(6,null))
+        }).toString())
+                .as("mergeKListsTest")
+                .isEqualTo(
+                        new ListNode(1,
+                                new ListNode(1,
+                                        new ListNode(2,
+                                                new ListNode(3,
+                                                        new ListNode(4,
+                                                                new ListNode(4,
+                                                                        new ListNode(5,
+                                                                                new ListNode(6,null)))))))).toString());
+    }
+
+    @Test
+    public void hasCycleTest(){
+        ListNode listNode1 = new ListNode(2,null);
+        ListNode listNode2 = new ListNode(0, new ListNode(-4, listNode1));
+        listNode1.next = listNode2;
+        assertThat(new HasCycle().hasCycle(new ListNode(3,listNode1)))
+                .as("hasCycleTest")
+                .isEqualTo(true);
+    }
+
+    @Test
+    public void detectCycleTest(){
+        ListNode listNode1 = new ListNode(2,null);
+        ListNode listNode2 = new ListNode(0, new ListNode(-4, listNode1));
+        listNode1.next = listNode2;
+        assertThat(new DetectCycle().detectCycle(new ListNode(3,listNode1)))
+                .as("detectCycleTest")
+                .isEqualTo(listNode1);
+    }
+
+    @Test
+    public void middleNodeTest(){
+
+        ListNode middleNode = new ListNode(3, new ListNode(4, new ListNode(5,null)));
+        ListNode listNode = new ListNode(1,new ListNode(2,middleNode));
+        assertThat(new MiddleNode().middleNode(listNode))
+                .as("middleNodeTest")
+                .isEqualTo(middleNode);
+    }
+
+    @Test
+    public void removeNthFromEndTest(){
+
+        assertThat(new RemoveNthFromEnd().removeNthFromEnd(new ListNode(1,new ListNode(2,new ListNode(3,new ListNode(4,new ListNode(5,null))))),2).toString())
+                .as("removeNthFromEndTest")
+                .isEqualTo(new ListNode(1,new ListNode(2,new ListNode(3,new ListNode(5,null)))).toString());
+    }
+
+    @Test
+    public void getIntersectionNodeTest(){
+
+        ListNode intersectionNode = new ListNode(8,new ListNode(4,new ListNode(5,null)));
+        assertThat(new GetIntersectionNode().getIntersectionNode(new ListNode(4,new ListNode(1,intersectionNode)),new ListNode(5,new ListNode(6,new ListNode(1,intersectionNode)))))
+                .as("getIntersectionNodeTest")
+                .isEqualTo(intersectionNode);
     }
 }
