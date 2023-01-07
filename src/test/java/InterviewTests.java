f286441399d662d8d5c76ab45cd98bc80dbc953e
jiangyaxin <792124098@qq.com>
2023-01-07 09:29:42 +0800

    新增面试题

---
+++ src/test/java/InterviewTests.java
@@ -0,0 +1,41 @@
+import interview.GenerateParenthesis;
+import interview.SpecialNumber;
+import interview.UncommonWord;
+import org.assertj.core.util.Lists;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author jiangyaxin
+ * @since 2022/7/27 16:21
+ */
+public class InterviewTests {
+
+    @Test
+    public void generateParenthesisTest(){
+        assertThat(new GenerateParenthesis().generateParenthesis(3))
+                .as("generateParenthesisTest")
+                .isEqualTo(Lists.list("((()))","(()())","(())()","()(())","()()()"));
+    }
+
+    @Test
+    public void isSpecialNumberTest(){
+        assertThat(new SpecialNumber().isSpecialNumber(19))
+                .as("isSpecialNumberTest")
+                .isEqualTo(true);
+        assertThat(new SpecialNumber().isSpecialNumber(2))
+                .as("isSpecialNumberTest")
+                .isEqualTo(false);
+    }
+
+    @Test
+    public void uncommonWordTest(){
+        assertThat(new UncommonWord().uncommonWord("this apple is sweet","this apple is sour"))
+                .as("uncommonWordTest")
+                .isEqualTo(new String[]{"sweet","sour"});
+        assertThat(new UncommonWord().uncommonWord("apple apple","banana"))
+                .as("uncommonWordTest")
+                .isEqualTo(new String[]{"banana"});
+    }
+}
